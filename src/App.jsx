1a2adad2c6c1f26c84f696a7145e916224c3f3ef
saigreeshma-06 <n210385@rguktn.ac.in>
2025-04-16 10:43:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import LoginPage from "./components/LoginPage";
 import SignupPage from "./components/SignupPage";
 import HomePage from "./components/HomePage";
 import Searchbar from "./components/Searchbar";
-
 import ProfilePage from "./components/ProfilePage";
 import BuyTicket from "./components/BuyTicket";
 import Seating from "./components/Seating";
+import Book_Summary from "./components/Book_Summary.jsx";
 
 
 const App = () => {
@@ -19,10 +19,10 @@ const App = () => {
       <Route path="/SignupPage" element={<SignupPage />} />
       <Route path="/HomePage" element={<HomePage />} />
       <Route path="/Searchbar" element={<Searchbar />} />
-      
       <Route path="/ProfilePage" element={<ProfilePage />} />
       <Route path="/BuyTicket" element={<BuyTicket />} />
       <Route path="/Seating" element={<Seating />} />
+      <Route path="/Book_Summary" element={<Book_Summary/>} />
       
     </Routes>
   );
